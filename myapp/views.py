164414f9a319878f3643890dc2c34cd233488efd
Solohiddin <164186798+solohiddin1@users.noreply.github.com>
2025-07-01 13:09:40 +0500

    Second commit

--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,5 @@ def home(request):
 def phones(request):
     phones = Phones.objects.all()
     return render(request,'phones.html',{'phones':phones})
+def about_us(request):
+    return render(request,'about_us.html')
